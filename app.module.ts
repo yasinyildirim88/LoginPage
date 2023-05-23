4bf281399dad9fb49eb08c06ec8f6ba882a889b8
Yasin YILDIRIM <122127619+yasinyildirim88@users.noreply.github.com>
2023-05-23 14:04:07 +0300

    Add files via upload

---
+++ app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { ForgotPasswordComponent } from './auth/forgot-password/forgot-password.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    ForgotPasswordComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
